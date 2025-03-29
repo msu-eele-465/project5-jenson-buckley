59a689ad473fa4decd053af1e27b9efd6d61aa3c
davidhjensen <dhjensen02@gmail.com>
2025-03-29 14:25:44 -0600

    working keypad read

---
+++ central/main.c
@@ -0,0 +1,138 @@
+#include <driverlib.h>
+
+void setupKeypad();
+char readKeypad();
+int checkRows();
+char lastKey = 'X';
+
+int main(void) {
+
+    volatile uint32_t i;
+    char pressed;
+
+    // Stop watchdog timer
+    WDT_A_hold(WDT_A_BASE);
+
+    // Set P1.0 to output direction
+    GPIO_setAsOutputPin(
+        GPIO_PORT_P1,
+        GPIO_PIN0
+        );
+
+    // Setup Keypad
+    setupKeypad();
+
+    // Disable the GPIO power-on default high-impedance mode
+    // to activate previously configured port settings
+    PMM_unlockLPM5();
+
+    
+    while(1)
+    {
+        pressed = readKeypad();
+        if (pressed=='1') {
+            // Toggle P1.0 output
+            
+            GPIO_toggleOutputOnPin(
+                GPIO_PORT_P1,
+                GPIO_PIN0
+                );
+        }
+    }
+}
+
+// --------------------------------------------- KEYPAD ---------------------------------------------
+void setupKeypad() {
+    // columns as outputs on P1.4, P5.3, P5.1, P5.0 initialized to 0
+    P1DIR |= BIT4;
+    P5DIR |= BIT3;
+    P5DIR |= BIT1;
+    P5DIR |= BIT0;
+    P1OUT &= ~BIT4;      
+    P5OUT &= ~BIT3;
+    P5OUT &= ~BIT1;
+    P5OUT &= ~BIT0;
+    // rows as inputs pulled down internally on P5.4, P1.1, P3.5, 3.1
+    P5DIR &= ~BIT4;     // inputs
+    P1DIR &= ~BIT1;
+    P3DIR &= ~BIT5;
+    P3DIR &= ~BIT1;
+    P5REN |= BIT4;      // internal resistors
+    P1REN |= BIT1;
+    P3REN |= BIT5;
+    P3REN |= BIT1;
+	P5OUT &=~ BIT4;     // pull-downs
+	P1OUT &=~ BIT1;
+	P3OUT &=~ BIT5;
+	P3OUT &=~ BIT1;
+}
+
+char readKeypad() {
+    // columns on P1.4, P5.3, P5.1, P5.0
+    // rows on P5.4, P1.1, P3.5, 3.1
+
+    char keys[4][4] = {
+        {'1', '2', '3', 'A'},
+        {'4', '5', '6', 'B'},
+        {'7', '8', '9', 'C'},
+        {'*', '0', '#', 'D'}
+    };
+
+    char pressed = 'X';
+
+    // check col 1
+    P1OUT |= BIT4;
+    int row = checkRows();
+    if (row!=-1) {
+        pressed = keys[row][0];
+    } 
+    P1OUT &= ~BIT4;
+
+    // check col 2
+    P5OUT |= BIT3;
+    row = checkRows();
+    row = checkRows();
+    if (row!=-1) {
+        pressed =  keys[row][1];
+    } 
+    P5OUT &= ~BIT3;
+
+    // check col 3
+    P5OUT |= BIT1;
+    row = checkRows();
+    if (row!=-1) {
+        pressed =  keys[row][2];
+    }
+    P5OUT &= ~BIT1;
+
+    // check col 4
+    P5OUT |= BIT0;
+    row = checkRows();
+    if (row!=-1) {
+        pressed =  keys[row][3];
+    }
+    P5OUT &= ~BIT0;
+
+    if (pressed != lastKey) {
+        lastKey = pressed;
+        return pressed;
+    } else {
+        return 'X';
+    }
+}
+
+int checkRows() {
+    // check inputs for rows on P5.4, P1.1, P3.5, 3.1 (rows 1-4)
+    if (P5IN & BIT4) {
+        return 0;
+    } else if (P1IN & BIT1) {
+        return 1;
+    } else if (P3IN & BIT5) {
+        return 2;
+    } else if (P3IN & BIT1) {
+        return 3;
+    } else {
+        return -1;
+    }
+}
+//---------------------------------------------
